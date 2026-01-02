710c741e09b1ff9f5b38bfe6d5c8c31570c3e2d2
SzymonTrojak <szymont888@gmail.com>
2026-01-02 18:30:36 +0100

    TEST_02

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import logging
 from flask import Flask
 import datetime
+from azure.monitor.opentelemetry import configure_azure_monitor
 app = Flask(__name__)
+
+
+# 1. Importuj funkcję konfiguracyjną
+
+
+# 2. Skonfiguruj Azure Monitor ZANIM stworzysz obiekt Flask
+# Upewnij się, że masz ustawioną zmienną środowiskową APPLICATIONINSIGHTS_CONNECTION_STRING
+# lub podaj ją bezpośrednio w connection_string="<twój_klucz>"
+configure_azure_monitor(
+    enable_live_metrics=True,  # TO JEST KLUCZOWE DLA LIVE METRICS
+)
+
 
 @app.route('/')
 def hello():
